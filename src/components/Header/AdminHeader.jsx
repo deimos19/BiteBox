a42ea7bedc66f49fea533cc06cbbb873f4ffd850
deimos19 <devadarshm2001@gmail.com>
2025-07-05 10:47:27 +0530

    modified file

--- src/components/Header/AdminHeader.jsx
+++ src/components/Header/AdminHeader.jsx
@@ -14,15 +14,9 @@ const AdminHeader = ({ toggleTheme, currentTheme }) => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <li><Link to={'/admin'}><i class="bi bi-house"></i>Home</Link></li>
+        <li><a href='#food'><i class="bi bi-tools"></i>Manage Food</a></li>
+        <li><Link to={'/admin/support'}><i class="bi bi-telephone"></i>Support</Link></li>
       </ul>
     </div>
     <img src={BiteBox} className='w-15 mr-3' alt="logo" />
