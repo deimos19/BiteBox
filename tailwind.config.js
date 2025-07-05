bf082d79aa13eac10835d0a267e5e23334f6279b
deimos19 <devadarshm2001@gmail.com>
2025-07-05 09:38:34 +0530

    modified file

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,6 @@ export default {
   plugins: [daisyui],
   daisyui: {
     themes: [
-      {
-        bitebox: {
-          "primary": "#F59E0B",
-          "secondary": "#FBBF24",
-          "accent": "#FB923C",
-          "neutral": "#1F2937",
-          "base-100": "#ffffff",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
-        },
-      },
       "light",
       "dark"
     ]
